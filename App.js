10d747a0279845408e996c2853249697200f7f9e
Ananya Radhakrishnan <ananyar807@gmail.com>
2024-04-06 11:50:14 -0400

    updated routes

--- App.js
+++ App.js
@@ -4,12 +4,14 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import QuizRoutes from "./Kanbas/quizzes/routes.js";
 const app = express();
 app.use(cors());
 
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+QuizRoutes(app);
 Lab5(app);
 Hello(app);
 
