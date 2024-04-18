4dadd22f2642c7137f10d688387490551bf57e62
taehyun <tanggu01@connect.hku.hk>
2024-04-17 22:33:18 -0400

    feat: add Questions routes

--- App.js
+++ App.js
@@ -4,12 +4,16 @@ import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
 import cors from "cors";
+import mongoose from "mongoose";
+import QuestionRoutes from "./Kanbas/Quizzes/Questions/routes.js";
 const app = express();
 app.use(cors());
 
 app.use(express.json());
+mongoose.connect("mongodb://127.0.0.1:27017/kanbas-project");
 ModuleRoutes(app);
 CourseRoutes(app);
+QuestionRoutes(app);
 Lab5(app);
 Hello(app);
 
