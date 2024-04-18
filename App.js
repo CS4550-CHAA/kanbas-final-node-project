b15fcc7376ef21ce4627ef4dfbbf2a7d914c1078
hams7504 <malli.hamsini@gmail.com>
2024-04-17 23:00:50 -0400

    added answers routes

--- App.js
+++ App.js
@@ -1,15 +1,19 @@
 import express from "express";
+import mongoose from "mongoose";
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5.js";
 import CourseRoutes from "./Kanbas/courses/routes.js";
 import ModuleRoutes from "./Kanbas/modules/routes.js";
+import AnswerRoutes from "./Kanbas/Quizzes/Answers/routes.js"
 import cors from "cors";
 const app = express();
 app.use(cors());
-
+const CONNECTION_STRING = process.env.local.DB_CONNECTION_STRING;
+mongoose.connect(CONNECTION_STRING);
 app.use(express.json());
 ModuleRoutes(app);
 CourseRoutes(app);
+AnswerRoutes(app);
 Lab5(app);
 Hello(app);
 
---
+++ Kanbas/Quizzes/Answers/dao.js
@@ -0,0 +1,8 @@
+import model from "./model.js";
+export const createAnswer = (answer) => {
+    return model.create(answer);
+  }
+export const findAnswerById = (answerId) => model.findById(answerId);
+export const findAllAnswers = (questionId) => model.find({ questionId: questionId });
+export const updateAnswer = (answerId, answer) =>  model.updateOne({ _id: answerId }, { $set: answer });
+export const deleteAnswer = (answerId) => model.deleteOne({ _id: answerId });
