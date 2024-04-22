d45f9ccd707aa33a03871252029b45da3526d084
hams7504 <malli.hamsini@gmail.com>
2024-04-21 20:35:57 -0400

    answer dao tweaks

--- Kanbas/Quizzes/Answers/routes.js
+++ Kanbas/Quizzes/Answers/routes.js
@@ -25,12 +25,13 @@ export default function UserRoutes(app) {
     };
     const updateAnswer = async (req, res) => {
         const { answerId } = req.params;
+        delete req.body._id;
         const status = await dao.updateAnswer(answerId, req.body);
         res.json(status);
       };
     app.post("/api/answers/:questionId", createAnswer);
     app.get("/api/answers/byQuestion/:questionId", findAllAnswers);
     app.get("/api/answers/:answerId", findAnswerById);
-    app.put("/api/answers/:questionId/:answerId", updateAnswer);
+    app.put("/api/answers/:answerId", updateAnswer);
     app.delete("/api/answers/:questionId/:answerId", deleteAnswer);
 }
